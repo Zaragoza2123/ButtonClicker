85690aaf90eca44a023c50460c06881f060e91d5
Zaragoza2123 <zaragoza.coyote618@gmail.com>
2022-01-28 13:54:44 -0600

    ButtonClicker

---
+++ btnclicker.js
@@ -0,0 +1,11 @@
+
+function logout(element) {
+    element.innerText = "Log out";
+}
+function disappear(element) {
+    element.remove();
+}
+function like(element) {
+    element.innerText = "14 likes";
+    alert("Ninja was liked");
+}
